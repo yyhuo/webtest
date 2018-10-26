aa1c72afecb7573ceb6da9d922881c08d48e2d83
yyhuo <szyuanyue@126.com>
2018-10-26 14:36:21 +0800

    第一次初始化

---
+++ webtest/function.py
@@ -0,0 +1,25 @@
+from django.http import HttpResponse
+from django.shortcuts import render
+
+
+def home(request):
+    return render(request, 'home.html')
+
+
+def count(request):
+    usr_text = request.GET['text']
+    total_count = len(request.GET['text'])
+    word_dict = {}
+    for word in usr_text:
+        if word not in word_dict:
+            word_dict[word] = 1
+        else:
+            word_dict[word] += 1
+    sorted_dict = sorted(word_dict.items(), key=lambda x: x[1], reverse=True)
+    return render(
+        request, 'count.html', {
+            'count': total_count,
+            'text': usr_text,
+            'dict': word_dict,
+            'sorted_dict': sorted_dict
+        })
